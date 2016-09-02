4652d4ad9b24c776e669976ad9bb1bf6b587fed5
stefan <stefan.graenitz@gmail.com>
2016-09-02 18:21:49 +0200

    Add a Interpreter to evaluate trees with a data-set regularly

---
+++ driver/utility/Interpreter.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "data/DecisionTree.h"
+#include "data/DecisionTreeNode.h"
+
+class Interpreter {
+public:
+    int64_t run(const DecisionTree &tree,
+                const std::vector<float> &dataSet) {
+      DecisionTreeNode node = tree.getRootNode();
+
+      while (!node.isImplicit()) {
+        float featureValue = dataSet[node.getFeatureIdx()];
+        NodeEvaluation eval = featureValue > node.getFeatureBias()
+                            ? NodeEvaluation::ContinueOneRight
+                            : NodeEvaluation::ContinueZeroLeft;
+
+        node = tree.getChildNodeFor(std::move(node), eval);
+      }
+
+      return node.getIdx();
+    }
+};
